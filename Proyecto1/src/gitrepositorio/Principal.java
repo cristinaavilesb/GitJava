6f97ad09c675f9e6706a9fb945084b85ef254a4a
Diurno <Diurno@pc0108.mshome.net>
2023-05-05 13:38:01 +0200

    Tercera confirmacion

--- Proyecto1/src/gitrepositorio/Principal.java
+++ Proyecto1/src/gitrepositorio/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Control de versiones con Git");
 		System.out.println("Nueva línea");
+		System.out.println("Hola Mundo");
 	}
 
 }
